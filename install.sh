577691561c4691de8fa2360fa7f3ee770278576f
shanesipe <shanesipe17@gmail.com>
2023-05-24 18:32:57 -0500

    Update install.sh
    Signed-off-by: shanesipe <shanesipe17@gmail.com>

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Update and Install Updates
-sudo apt update 
-sudo apt upgrade -y
+# Install Apache, MariaDB, PHP 8.1, and unzip  
+sudo apt install apache2 mariadb-server php8.1 unzip php8.1-mbstring
 
-# Install Apache, MariaDB, PHP 8.1, and unzip
-sudo apt install apache2 mariadb-server php8.1 unzip 
+# Enable Apache rewrite module  
+sudo a2enmod rewrite  
 
-# Start Apache and MariaDB 
-sudo systemctl start apache2 
-sudo systemctl start mariadb
+# Start Apache and MariaDB
+sudo systemctl start apache2  
+sudo systemctl start mariadb  
 
 # Run MariaDB secure installation
-sudo mysql_secure_installation
+sudo mysql_secure_installation  
 
 # Prompt for OpenEMR database name, username, and password
-read -p "Enter OpenEMR database name: " dbname  
+read -p "Enter OpenEMR database name: " dbname   
 read -p "Enter OpenEMR database username: " dbuser
 read -p "Enter OpenEMR database password: " dbpass
 
@@ -23,24 +22,24 @@ read -p "Enter OpenEMR database password: " dbpass
 mysql -u root -p -e "CREATE DATABASE $dbname; CREATE USER '$dbuser' IDENTIFIED BY '$dbpass'; GRANT ALL ON $dbname.* TO '$dbuser';"
 
 # Prompt for admin username and password
-read -p "Enter OpenEMR admin username: " admin_user  
+read -p "Enter OpenEMR admin username: " admin_user   
 read -p "Enter OpenEMR admin password: " admin_pass
 
 # Prompt for hostname
-read -p "Enter your server's hostname: " hostname  
+read -p "Enter your server's hostname: " hostname   
 
 # Install PHP extensions
-sudo apt install php8.1-mysql php8.1-gd php8.1-xml php8.1-curl php8.1-zip php8.1-mbstring
+sudo apt install php8.1-mysql php8.1-gd php8.1-xml php8.1-curl php8.1-zip   
 
 # Activate extensions
 sudo phpenmod mysql gd xml curl zip mbstring
 
-# Edit PHP config 
-sudo nano /etc/php/8.1/apache2/php.ini
+# Edit PHP config
+sudo nano /etc/php/8.1/apache2/php.ini   
 
-# Set values
-short_open_tag = Off  
-display_errors = Off 
+# Set values  
+short_open_tag = Off   
+display_errors = Off
 register_globals = Off
 max_input_vars = 3000
 max_execution_time = 60
@@ -50,8 +49,29 @@ memory_limit = 256M
 mysqli.allow_local_infile = On
 
 # Save and exit 
-ctrl + o 
+ctrl + o
 ctrl + x
+
+# Edit Apache config
+sudo nano /etc/apache2/apache2.conf
+
+# Add lines
+<Directory "/var/www/html/openemr">
+                AllowOverride FileInfo=All
+                Require all granted
+</Directory>  
+
+<Directory "/var/www/html/openemr/sites">
+                AllowOverride None
+</Directory>
+
+<Directory "/var/www/html/openemr/sites/*/documents">
+                Require all denied
+</Directory>
+
+# Save and exit
+ctrl + o  
+ctrl + x   
 
 # Restart Apache
 sudo systemctl restart apache2
@@ -63,22 +83,16 @@ wget https://sourceforge.net/projects/openemr/files/latest/download -O openemr.z
 unzip openemr.zip
 
 # Move to Apache document root
-sudo mv openemr-7.0.1 openemr
-
 sudo mv openemr /var/www/html/
 
 # Import SQL schema
-mysql -u $dbuser -p $dbname < /var/www/html/openemr/sql_upgrade.sql 
+mysql -u $dbuser -p $dbname < /var/www/html/openemr/sql_upgrade.sql
+
+# Open in web browser and complete setup
+echo "OpenEMR installed! Access at http://$hostname/openemr"
+echo "Complete the setup wizard. Then login with admin username '$admin_user' and password '$admin_pass'"
 
 # Configure OpenEMR after setup
 sudo nano /var/www/html/openemr/sites/default/config.php
 # Set values for $host, $port, $login, $pass, $dbase
 
-# Save and exit 
-ctrl + o 
-ctrl + x
-
-
-# Open in web browser and complete setup
-echo "OpenEMR installed! Access at http://$hostname/openemr"
-echo "Complete the setup wizard. Then login with admin username '$admin_user' and password '$admin_pass'"
